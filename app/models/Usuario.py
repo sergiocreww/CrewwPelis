ae0bc9fdd88f2714af209493c1f3e71242d62278
sergiocreww <sssssscccccc111@gmail.com>
2024-02-01 23:33:10 -0500

    commit en casa

--- app/models/Usuario.py
+++ app/models/Usuario.py
@@ -1,6 +1,7 @@
 from app import db
 
 class Usuario(db.Model):
+    __tablename__ = "usuario"
     idUsuario = db.Column(db.Integer, primary_key=True)
     NombreUsuario = db.Column(db.String(50), nullable=False)
     ContrasenaUsuario = db.Column(db.String(20), nullable=False)
