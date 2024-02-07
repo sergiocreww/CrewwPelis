73ef0ce6ac1e76896a08ac9eb14baa57c6044461
sergiocreww <sssssscccccc111rgmail.com>
2024-02-07 11:46:00 -0500

    Commit en el Sena 3.0

--- app/routes/Usuario_routes.py
+++ app/routes/Usuario_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, session
 from app.models.Usuario import Usuario
 from app import db
 
@@ -18,12 +18,30 @@ def Registro():
         ContrasenaUsuario = request.form['Contrasena']
 
         new_usuario = Usuario(NombreUsuario=NombreUsuario, ContrasenaUsuario=ContrasenaUsuario)
-        db.session.Registro(new_usuario)
+        db.session.add(new_usuario)
         db.session.commit()
  
         return redirect(url_for('usuario.Registro'))
     
     return render_template('Usuarios/Registro.html')
+
+@bp.route('/Usuario/Login', methods=['GET' , 'POST'])
+def Login():
+    if request.method == 'POST':
+        NombreUsuario = request.form['Nombre']
+        ContrasenaUsuario = request.form['Contrasena']
+
+        usuario = Usuario.query.filter_by(NombreUsuario=NombreUsuario, ContrasenaUsuario=ContrasenaUsuario).first()
+
+        if usuario:
+            # Iniciar sesión y redirigir a la página principal (o a donde desees)
+            session['idUsuario'] = usuario.idUsuario
+            return render_template('Usuarios/index2.html')
+        else:
+            # Mostrar un mensaje de error si las credenciales son incorrectas
+            error = 'Credenciales incorrectas. Inténtalo de nuevo.'
+            return render_template('Usuarios/Registro.html', error=error)
+
 
 
 
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import os
 
 app = create_app()
 
+
 with app.app_context():
     db.create_all()
 
