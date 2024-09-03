075e2368718e55ec8e2ce042db73648668738e9f
sergiocreww <sssssscccccc111@gmail.com>
2024-09-03 11:31:26 -0500

    archivo config

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    # Puedes eliminar o comentar las siguientes líneas si no necesitas una base de datos
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:eed12Hhc6244HdAaBCfgCA-Dfa55dc3g@viaduct.proxy.rlwy.net:59128/peliculas'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Otras configuraciones que podrías necesitar
+    DEBUG = True  # O False en producción
