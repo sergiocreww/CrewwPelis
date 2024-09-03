ca708f2c370f305cec965c343a8edeb774154cbd
sergiocreww <sssssscccccc111@gmail.com>
2024-09-03 11:25:48 -0500

    eliminacion de ruta a la base de datos

--- config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:eed12Hhc6244HdAaBCfgCA-Dfa55dc3g@viaduct.proxy.rlwy.net:59128/peliculas'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
